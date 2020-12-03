eac65027e1fa79e0e3d4da7ff6646eccbd10a2e0
Ricardo Jesús Cabrera Valero <a.ricardocabrera@gmail.com>
2020-12-03 20:04:46 +0100

    Formulario de citas

---
+++ ReactJS/citas/src/components/Formulario.js
@@ -0,0 +1,101 @@
+import React, { Fragment, useState } from 'react';
+
+const Formulario = () => {
+
+    //Crear State de Citas
+    const [cita, actualizarCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: ''
+    })
+
+    //Función que se ejecuta cada vez que el usuario escribe un input
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //Extraer los valores
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+    //Cuando el usuario envia el formulario
+    const submitCita = e => {
+        e.preventDefault();
+
+        // Validar
+
+        // Asignar un ID
+
+        // Crear la cita
+
+        // Reiniciar el form
+
+    }
+
+    return (
+        <Fragment>
+            <h2>Crear Cita</h2>
+
+            <form
+                onSubmit={submitCita}
+            >
+                <label>Nombre Mascota</label>
+                <input
+                    type="text"
+                    name="mascota"
+                    className="u-full-width"
+                    placeholder="Nombre mascota"
+                    onChange={actualizarState}
+                    value={mascota}
+                />
+
+                <label>Nombre Dueño</label>
+                <input
+                    type="text"
+                    name="propietario"
+                    className="u-full-width"
+                    placeholder="Nombre Dueño de la mascota"
+                    onChange={actualizarState}
+                    value={propietario}
+                />
+
+                <label>Fecha</label>
+                <input
+                    type="date"
+                    name="fecha"
+                    className="u-full-width"
+                    onChange={actualizarState}
+                    value={fecha}
+                />
+
+                <label>Hora</label>
+                <input
+                    type="time"
+                    name="hora"
+                    className="u-full-width"
+                    onChange={actualizarState}
+                    value={hora}
+                />
+
+                <label>Síntomas</label>
+                <textarea
+                    name="sintomas"
+                    className="u-full-width"
+                    onChange={actualizarState}
+                    value={sintomas}
+                ></textarea>
+
+                <button
+                    type="submit"
+                    className="u-full-width button-primary"
+                >Agregar Cita</button>
+            </form>
+        </Fragment>
+    );
+}
+
+export default Formulario;
